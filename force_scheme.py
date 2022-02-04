b0630c931d07fd71e9d397275b78bed9883274b3
Fernando V. Paulovich <paulovich@dal.ca>
2022-02-04 13:33:58 -0500

    Changing to follow scikit-learn template as best as possible.

--- force_scheme.py
+++ force_scheme.py
@@ -1,60 +1,28 @@
 import numpy as np
 from numpy import random
 import math
-from numba import jit
-from distance_matrix import DistanceMatrix
+from sklearn import decomposition
 
-import distance_matrix as dm
+from sklearn.datasets import load_iris
 import matplotlib.pyplot as plt
 
-from timeit import default_timer as timer
-from datetime import timedelta
+from distance_matrix import DistanceMatrix
+from scipy.spatial import distance
+import distance_matrix as dm
 
 
 class ForceScheme:
 
-    def __init__(self):
-        self.max_it_ = 100
-        self.learning_rate0_ = 0.5
-        self.decay_ = 0.95
+    def __init__(self,
+                 max_it=100,
+                 learning_rate0=0.5,
+                 decay=0.95):
 
-        # create the initial projection
-        self.projection_ = None
+        self.max_it_ = max_it
+        self.learning_rate0_ = learning_rate0
+        self.decay_ = decay
+        self.embedding_ = None
 
-        # pca = decomposition.PCA(n_components=2).fit(X)
-        # Y = pca.transform(X)
-
-    def execute(self, dataset, distance_function):
-        # create distance matrix
-        distance_matrix = dm.DistanceMatrix(dataset, distance_function)
-        return self.execute(distance_matrix)
-
-    def execute(self, distance_matrix: DistanceMatrix):
-        # create the initial projection
-        self.projection_ = np.random.random((distance_matrix.size(), 2))
-
-        for k in range(self.max_it_):
-            start = timer()
-
-            learning_rate = self.learning_rate0_ * math.pow((1 - k / self.max_it_), self.decay_)
-            error = self.iteration(learning_rate, distance_matrix)
-
-            end = timer()
-            print('Force iteration #{0}-{1} time: {2} error:{3}'.format((k + 1),
-                                                                        self.max_it_,
-                                                                        timedelta(seconds=end - start),
-                                                                        error))
-
-        # setting the min to (0,0)
-        min_x = min(self.projection_[:, 0])
-        min_y = min(self.projection_[:, 1])
-        for i in range(len(self.projection_)):
-            self.projection_[i][0] -= min_x
-            self.projection_[i][1] -= min_y
-
-        return self.projection_
-
-    @jit
     def iteration(self, learning_rate: float, distance_matrix: DistanceMatrix):
         error = 0.0
         size = distance_matrix.size()
@@ -69,7 +37,6 @@ class ForceScheme:
 
         return error / size
 
-    @jit
     def move(self, pivot, learning_rate: float, distance_matrix: DistanceMatrix):
         error = 0.0
         size = distance_matrix.size()
@@ -77,8 +44,8 @@ class ForceScheme:
         for ins in range(size):
 
             if pivot != ins:
-                x1x2 = self.projection_[ins][0] - self.projection_[pivot][0]
-                y1y2 = self.projection_[ins][1] - self.projection_[pivot][1]
+                x1x2 = self.embedding_[ins][0] - self.embedding_[pivot][0]
+                y1y2 = self.embedding_[ins][1] - self.embedding_[pivot][1]
                 dr2 = math.sqrt(x1x2 * x1x2 + y1y2 * y1y2)
 
                 if dr2 < 0.0001:
@@ -92,79 +59,52 @@ class ForceScheme:
                 error += math.fabs(delta)
 
                 # moving
-                self.projection_[ins][0] = self.projection_[ins][0] + learning_rate * delta * (x1x2 / dr2)
-                self.projection_[ins][1] = self.projection_[ins][1] + learning_rate * delta * (y1y2 / dr2)
+                self.embedding_[ins][0] = self.embedding_[ins][0] + learning_rate * delta * (x1x2 / dr2)
+                self.embedding_[ins][1] = self.embedding_[ins][1] + learning_rate * delta * (y1y2 / dr2)
 
         return error / size
 
+    def _fit(self, X, y, distance_matrix):
+        # set the initial projection
+        if y is None:
+            pca = decomposition.PCA(n_components=2).fit(X)
+            self.embedding_ = pca.transform(X)
+        else:
+            self.embedding_ = y
+
+        for k in range(self.max_it_):
+            learning_rate = self.learning_rate0_ * math.pow((1 - k / self.max_it_), self.decay_)
+            error = self.iteration(learning_rate, distance_matrix)
+
+        # setting the min to (0,0)
+        min_x = min(self.embedding_[:, 0])
+        min_y = min(self.embedding_[:, 1])
+        for i in range(len(self.embedding_)):
+            self.embedding_[i][0] -= min_x
+            self.embedding_[i][1] -= min_y
+
+        return self.embedding_
+
+    def fit_transform(self, X, y=None, distance_function=distance.euclidean):
+        distance_matrix = dm.DistanceMatrix().fit_transform(X, distance_function)
+        return self._fit(X, y, distance_matrix)
+
+    def fit(self, X, y=None, distance_function=distance.euclidean):
+        distance_matrix = dm.DistanceMatrix().fit_transform(X, distance_function)
+        return self._fit(X, y, distance_matrix)
+
 
 def main():
-    # exemplos de funcao de distancia
-    # https://docs.scipy.org/doc/scipy/reference/spatial.distance.html
 
-    # # input_file = "/Users/fpaulovich/Dropbox/datasets/data/19k_fourier30_spatial10_normalized.csv"
-    # # input_file = "/Users/fpaulovich/Dropbox/datasets/data/segmentation-normcols.csv"
-    # input_file = "/Users/fpaulovich/Dropbox/datasets/data/mammals-50000.bin-normcols.csv"
-    #
-    # dataset = pd.read_csv(input_file)
-    # labels = dataset.loc[:, 'label']
-    # dataset = dataset.drop(['id', 'label'], axis=1) #remove ids and labels
-    #
-    # print(dataset)
-    #
-    # force = ForceScheme(dataset)
-    # force.max_it = 10
-    # projection = force.execute(distance.euclidean)
-    #
-    # plt.figure()
-    # plt.scatter(projection[:, 0], projection[:, 1], c=labels,
-    #             cmap='Set2', edgecolors='black', linewidths=0.5)
-    # plt.grid(linestyle='dotted')
-    # plt.show()
-
-    ####################
-
-    # input_file = "/Users/fpaulovich/Dropbox/datasets/data/segmentation-normcols.csv"
-    # dataset = pd.read_csv(input_file)
-    # labels = dataset.loc[:, 'label']
-    #
-    # input_file = "/Users/fpaulovich/Desktop/segmentation_distance_matrix.txt"
-    # dmat = dm.DistanceMatrix().read(input_file)
-    #
-    # force = ForceScheme()
-    # force.max_it_ = 5
-    # projection = force.execute(dmat)
-    #
-    # plt.figure()
-    # plt.scatter(projection[:, 0], projection[:, 1], c=labels,
-    #             cmap='tab10', edgecolors='face', linewidths=0.5, s=4)
-    # plt.grid(linestyle='dotted')
-    # plt.show()
-
-    #####################
-
-    start = timer()
-
-    # input_file = "/Users/fpaulovich/Desktop/matriz_distancia"
-    input_file = "/Users/fpaulovich/Documents/protein_folding/dmat_8000"
-    dmat = dm.DistanceMatrix().read(input_file)
-
-    force = ForceScheme()
-    force.max_it_ = 10
-    projection = force.execute(dmat)
-
-    end = timer()
-    print('Total time: ', timedelta(seconds=end - start))
-
-    labels = np.zeros(len(projection))
+    raw = load_iris(as_frame=True)
+    X = raw.data.to_numpy()
+    y = ForceScheme().fit_transform(X)
 
     plt.figure()
-    plt.scatter(projection[:, 0], projection[:, 1], c=labels,
+    plt.scatter(y[:, 0], y[:, 1], c=raw.target,
                 cmap='tab10', edgecolors='face', linewidths=0.5, s=4)
     plt.grid(linestyle='dotted')
     plt.show()
-
-    ####################
 
     return
 
