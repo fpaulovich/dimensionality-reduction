51bcccaa8e6f33c6cad9eac11e92160d4e079ef6
Fernando Vieira Paulovich <70700238+fpaulovich@users.noreply.github.com>
2022-02-28 15:33:12 -0400

    fixing an small bug when defining the dummy points

--- dgrid.py
+++ dgrid.py
@@ -49,8 +49,8 @@ class DGrid:
         bounding_box_height = max_coordinates[1] - min_coordinates[1]
 
         # defining the number of rows and columns
-        nr_columns = int((self.delta_ * bounding_box_width) / self.icon_width_)
-        nr_rows = int((self.delta_ * bounding_box_height) / self.icon_height_)
+        nr_columns = math.ceil((self.delta_ * bounding_box_width) / self.icon_width_)
+        nr_rows = math.ceil((self.delta_ * bounding_box_height) / self.icon_height_)
 
         # if the number of rows and columns are not enough to fit all data instances, increase delta
         if nr_rows * nr_columns < len(y):
