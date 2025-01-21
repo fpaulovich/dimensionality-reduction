d14343a4ad9dc1ed131370011205a6e302d12ed2
fpaulovich <f.paulovich@tue.nl>
2025-01-21 17:23:49 +0100

    Removing parallelism of LAMP since it is slower

--- lamp/lamp_main.py
+++ lamp/lamp_main.py
@@ -21,51 +21,6 @@ from sklearn.manifold import TSNE
 import random
 import math
 
-import pandas as pd
-import numpy as np
-
-
-def main_song():
-    data_file = "../data/song_data.csv"
-    df = pd.read_csv(data_file, header=0, engine='python')
-
-    df = df.sort_values(by='song_popularity', ascending=True)
-    df = df.drop_duplicates('song_name', keep='last')  # drop duplicates, keep the largest song_popularity
-
-    label = np.array(df['song_popularity'].values).reshape(-1, 1)
-    label = preprocessing.MinMaxScaler().fit_transform(label)[:, 0]
-
-    df = df.drop(['song_name', 'song_popularity'], axis=1)
-    X = df.values
-    X = preprocessing.MinMaxScaler().fit_transform(X)
-
-    # define sample size
-    sample_size = int(len(X) * 0.5)
-
-    # get a random sample
-    random.seed(7)
-    X_sample = X[random.sample(range(len(X)), sample_size), :]
-
-    # project the sample
-    y_sample = TSNE(n_components=2,
-                    perplexity=10,
-                    random_state=0).fit_transform(X_sample)
-
-    start = timer()
-    lamp = Lamp(nr_neighbors=10).fit(X_sample=X_sample,
-                                     y_sample=y_sample)
-    y = lamp.transform(X=X)
-    end = timer()
-
-    print('Lamp took {0} to execute'.format(timedelta(seconds=end - start)))
-
-    plt.figure()
-    plt.scatter(y[:, 0], y[:, 1], c=label, cmap='viridis', edgecolors='face', linewidths=0.5, s=4)
-    plt.grid(linestyle='dotted')
-    plt.show()
-
-    return
-
 
 def main_tsne():
     raw = datasets.load_digits(as_frame=True)
@@ -73,7 +28,7 @@ def main_tsne():
     X = preprocessing.StandardScaler().fit_transform(X)
 
     # define sample size
-    sample_size = int(len(X) * 0.5)
+    sample_size = int(len(X) * 0.25)
 
     # get a random sample
     random.seed(7)
@@ -131,5 +86,5 @@ def main_force():
 
 
 if __name__ == "__main__":
-    main_song()
+    main_tsne()
     exit(0)
